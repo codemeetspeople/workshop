88075ac7e732f1dccae2910fa2c7ddfd3a38fd06
Oleksandr Kunzietsov <caiman@7sky.dev>
2024-08-10 14:00:52 +0300

    Base

--- base.c
+++ base.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
 
+int getDigits(int value, int base) {
+    int result = 0;
+
+    for ( ; value > 0; value /= base, result++ );
+    return result;
+}
+
+int getOrder(int count, int base) {
+    int result = 1;
+
+    for ( int i = 1; i < count; i++, result *= base);
+    return result;
+}
+
 int main() {
     int target, base;
-    int result = 0;
+    int digits;
 
     scanf("%d %d", &target, &base);
 
-    for ( ; target > 0; target /= base ) {
-        result += 1;
+    digits = getDigits(target, base);
+
+    for ( int i = digits; i > 0; i-- ) {
+        int order = getOrder(i, base);
+        int value = target / order;
+
+        printf("%d", value);
+        target -= order * value;
     }
-    printf("%d\n", result);
+    printf("\n");
+    
 
     return 0;
 }
